57e4cb2d4fc3bcce53d482d0e1eb39c420e893f2
Pedro Henrique Rissato <pedro_giberti@hotmail.com>
2020-03-26 00:33:38 -0300

    Update routes.js

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,7 +1,5 @@
 import { createAppContainer, createSwitchNavigator} from 'react-navigation';
 
-//Existem vários tipos de navegação. Na documentação do react navigation pode consutlar. a Switch navigation é uma delas
-
 import Login from './pages/Login';
 import List from './pages/List';
 import Book from './pages/Book';
