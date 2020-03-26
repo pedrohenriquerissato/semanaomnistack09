a90449786eddb0d22e2eb60e390ee25e1fff64fd
Pedro Henrique Rissato <pedro_giberti@hotmail.com>
2020-03-26 00:28:56 -0300

    Update RejectionController.js

--- backend/src/controllers/RejectionController.js
+++ backend/src/controllers/RejectionController.js
@@ -10,13 +10,9 @@ module.exports = {
 
         await booking.save();
 
-        // Regra de Negocio para mostrar a reserva em tempo real.
-        // A ideia é buscar o dono do "spot" e ver se ele está logado no momento.
         const bookingUserSocket = req.connectedUsers[booking.user];
 
-        // Se o dono do Spot estiver conectado, vou enviar uma mensagem pra ele
         if (bookingUserSocket){
-            // Pega o req com o io e envia uma mensagem com o objeto booking ao Dono do Spot
             req.io.to(bookingUserSocket).emit('booking_response', booking);
         }
 
